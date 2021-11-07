120b54931be92c2b5617eb12edf811cf98b46647
Daniel Lasek <deroxon@gmail.com>
2021-11-07 21:25:36 +0100

    added system of checking condition if hero is getting upgrade or punish in brief is he fulfills the condition, also implement a upgrade hero stats function

--- src/fakeData/fakeAnswers.js
+++ src/fakeData/fakeAnswers.js
@@ -7,6 +7,7 @@ let fakeAnswers= [
             stats: {hp: 0, att: 1, def: 0, coin: 0},
             storyline: "You choosed Sword which one is making you stronger, you get 1 attack point",
             condition: "hp1",
+            penalty: {hp: 0, att: 0, def: 0, coin: 0},
             checkDec: ''
         },
         B: {
@@ -14,6 +15,7 @@ let fakeAnswers= [
             stats: {hp: 0, att: 0, def: 1, coin: 0},
             storyline: "You choosed helmet which one is making you safer, you get 1 deffense point",
             condition: "hp1",
+            penalty:  {hp: 0, att: 0, def: 0, coin: 0},
             checkDec: ''
         },
 
@@ -22,6 +24,7 @@ let fakeAnswers= [
             stats: {hp: 0, att: 0, def: 1, coin: 0},
             storyline: "You choosed shield which one is making you safer, you get 1 deffense point",
             condition: "hp1",
+            penalty:  {hp: 0, att: 0, def: 0, coin: 0},
             checkDec: ''
         },
         result: ''
--- src/mainScripts/chatgame.js
+++ src/mainScripts/chatgame.js
@@ -8,32 +8,127 @@ function ChatGame() {
     const [story, setStory] = useState('');
     const [answers, setAnswers] = useState({A: '', B: '', C: ''})
     const [game, setGame] = useState();
+    const [hero, setHeroStats] = useState({stats: {
+        attack: 1,
+        deffense: 1,
+        healthPoints: 3,
+        coin: 3, 
+    }, 
+    backpack : [], 
 
+    decisions: {
+
+    }
+    })
+
+    // mount first story, answers and game options
     useEffect( ()=> {
         setStory(fakeAnswers[0].story);
         setAnswers({A: fakeAnswers[0].A.content , B: fakeAnswers[0].B.content,C: fakeAnswers[0].C.content});
         setGame(fakeAnswers[0]);
     },[] )
 
-    console.log(game)
+  
 
     function makeDecision(param) {
 
+        let condition = false;
+        
+        // decision A, B, C checking condition if its true upgrade hero, and if false hero need to take punish 
+        
         if(param === 'A') {
-            
+            if(checkCondition(game.A.condition)) {
+                upgradeHero(game.A.stats);
+            } else {
+                punishHero(param)
+            }
+
         } 
         
         else if (param === 'B') {
+            
+            if(checkCondition(game.B.condition)) {
+                upgradeHero(game.B.stats);
+            } else {
+                punishHero(param)
+            }
 
         }
 
         else if (param === 'C') {
+
+            if(checkCondition(game.C.condition)) {
+                upgradeHero(game.C.stats);
+            } else {
+                punishHero(param)
+            }
          
         }
 
     }
 
-    function checkCondition() {
+    // decision to think
+    function checkCondition(condition) {
+
+        console.log(condition);
+        // attribute
+        let takeWhat = condition.slice(0,2);
+        // value
+        let takeCon = condition.slice(2,5);
+        takeCon = Number(takeCon);
+
+        // destrukturyzacja w obiekcie hero, obiektu stats
+        let {healthPoints, attack, deffense, coins } = hero.stats;
+
+        switch (takeWhat) {
+
+            //healthpoints
+            case 'hp': 
+                if(healthPoints >= takeCon) {
+                    return true;
+                } else {
+                    return false;
+                }
+            break;
+
+            // attack
+            case 'at': {
+                if(attack >= takeCon) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            break;
+
+            //deffens
+            case 'def': {
+                if(deffense >= takeCon) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            break;
+
+            //coin
+            case 'co': {
+                if(coins >= takeCon) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            break
+
+            //decision i need to think how decision will work
+            case 'dc': {
+
+            }
+        }
+
+        
+        
 
     }
 
@@ -41,8 +136,27 @@ function ChatGame() {
 
     }
 
-    function upgradeHero() {
+    function upgradeHero(...param) {
+
+        console.log(param[0])
+        // setting variables which one making new values in heros stats
+        let newAttack = param[0].att + hero.stats.attack;
+        let newDeffense = param[0].def + hero.stats.deffense;
+        let newHealthPoints = param[0].hp + hero.stats.healthPoints;
+        let newCoin= param[0].coin + hero.stats.coin;
         
+        // setting new values to hero
+        setHeroStats({stats: {
+            attack: newAttack, 
+            deffense: newDeffense, 
+            healthPoints: newHealthPoints, 
+            coin: newCoin
+        }})
+
+        
+    }
+    function punishHero(param) {
+
     }
 
  
@@ -54,7 +168,7 @@ function ChatGame() {
 
                     <div className="communicate">alert</div>  
                     
-                    <div className="stats">att deff, coin, bacfasdfsadfsdk</div>
+                    <div className="stats">{"att:"+hero.stats.attack} {"def:"+hero.stats.deffense}, {"coin:"+hero.stats.coin},{"hp:"+hero.stats.healthPoints}</div>
                     
                 
                 
